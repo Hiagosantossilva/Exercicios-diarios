dd5868c0e5c4f805c92b717428a879e31c3ab84a
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2025-08-03 23:36:31 -0300

    treino

--- Javascript/03_08_2025 Treino/script.js
+++ Javascript/03_08_2025 Treino/script.js
@@ -1,2 +1,19 @@
 const mais = document.getElementById('mais');
 const menos = document.getElementById('menos'); 
+var contador = 0;
+
+mais.addEventListener('click', () => {
+    contador = contador + 1;  
+    const resposta = document.getElementById('res');
+    resposta.innerHTML = contador;
+})
+
+menos.addEventListener('click', () => {
+    if (contador > 0) {
+        contador = contador - 1;  
+    } else {
+        contador = 0
+    }
+    const resposta = document.getElementById('res');
+    resposta.innerHTML = contador;
+})
