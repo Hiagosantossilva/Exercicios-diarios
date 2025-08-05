a34b6998a233386cc98a13352d23d4a39569a790
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2025-08-04 21:41:24 -0300

    Exercício Flask

---
+++ PHP/Exercicio 04_08_2025/routes.py
@@ -0,0 +1,14 @@
+from main import app
+from flask import render_template 
+
+# rotas
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return 'Segunda página'
+
+
