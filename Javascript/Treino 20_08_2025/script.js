cae7595d0613b0d6d968fe80e36f629d04cadb9e
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2025-08-20 23:26:48 -0300

    exercicio

---
+++ Javascript/Treino 20_08_2025/script.js
@@ -0,0 +1,5 @@
+const aparecer = document.getElementById('aparecer');
+aparecer.addEventListener('click', () => {
+    const res = document.getElementById('res');
+    res.innerHTML = 'Apareceu!';
+})
