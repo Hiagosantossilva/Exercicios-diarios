58d61feb893373582eabb36f001ad6eef71ab7a5
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2025-09-10 19:44:41 -0300

    atualizacao netflix

--- Javascript/06_09_2025/codeflix/src/App.js
+++ Javascript/06_09_2025/codeflix/src/App.js
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+
 
 export default () => {
 
-  const [movieList, setMovieList] = useEffect([]);
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
 
     }
+    loadAll();
   }, []);
   
   return (
     <div className="page">
-      Olá Mundo!
+      <section className ="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title ={item.title} items={item.items}/>
+        ))}
+
+      </section>
     </div>
   );
 }
