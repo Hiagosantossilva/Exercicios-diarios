8a62639ac339ebed1cfc4a243be0364337270225
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2025-09-29 23:01:06 -0300

    new

--- Javascript/06_09_2025/codeflix/src/App.js
+++ Javascript/06_09_2025/codeflix/src/App.js
@@ -3,11 +3,14 @@ import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header'
+import { Widgets } from '@mui/icons-material';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState([]);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -25,9 +28,25 @@ export default () => {
     }
     loadAll();
   }, []);
+
+  useEffect(()=>{
+    const scrollListener = () =>{
+        if(window.scrollY > 10){
+          setBlackHeader(true);
+        } else{
+          setBlackHeader(false)
+        }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }}
+  )
   
   return (
     <div className="page">
+      <Header black={blackHeader}/>
+
       {featuredData &&
           <FeaturedMovie item={featuredData}/>
       }
@@ -38,6 +57,18 @@ export default () => {
         ))}
 
       </section>
+      <footer>
+        Feito com <span role="img" arial-label="coração">❤️ por Hiago Santos Silva</span><br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
+      {movieList.length <= 0 && 
+       <div className="loading">
+        <img src="https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyeW5mcjk0ajdqYWk1dWZoZzE0ajcwdzdtODk5MmJoenk1a2lkemtzeiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/FgH5xSNjGHZsiYPWAX/giphy.gif" ></img>
+      </div>
+      
+      }
+     
     </div>
   );
 }
