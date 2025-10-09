ab3103076b0451fabb47a4f22ac9b25f873c9e1d
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2025-10-08 21:38:02 -0300

    new

---
+++ Javascript/08_10_2025/script.js
@@ -0,0 +1,8 @@
+const botao = window.document.getElementById('color').addEventListener('click', ()=>
+{
+    const body = document.body
+    const h1 = document.querySelector('h1');
+
+    body.style.background = "black";
+    h1.style.color = "white";
+})
