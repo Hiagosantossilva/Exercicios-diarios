1506d392250a824d3a4eb80cbfbc6b22878142d2
Hiagosantossilva <102103266+Hiagosantossilva@users.noreply.github.com>
2026-01-07 09:10:19 -0300

    3.1.4

---
+++ TypeScript/30_12_2025.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=30_12_2025.d.ts.map
---
+++ TypeScript/30_12_2025.ts
@@ -0,0 +1,8 @@
+let age: number = 5
+
+const helloWorld = (num1: number, num2: number) => {
+    return num1 + num2
+}
+
+helloWorld(1, 2);
+    
